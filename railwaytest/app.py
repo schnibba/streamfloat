0c194a00f058d5d89f253ddcad0466c3c3c8f262
schnibba <89694678+schnibba@users.noreply.github.com>
2025-09-15 20:29:54 +0200

    Update app.py

--- railwaytest/app.py
+++ railwaytest/app.py
@@ -2,14 +2,12 @@ from fastapi import FastAPI
 from scrapers import soundcloud_7Dstreams, spotify_7Dstreams
 import asyncio
 
-app = FastAPI()
-
-@app.post("/run-scraper1")
-async def run_scraper1():
-    await soundcloud_7Dstreams.main()
+@app.post("/scrape/spotify")
+async def scrape_spotify():
+    await spotify_scraper.main()
     return {"status": "ok"}
 
-@app.post("/run-scraper2")
-async def run_scraper2():
-    await spotify_7Dstreams.main()
+@app.post("/scrape/soundcloud")
+async def scrape_soundcloud():
+    await soundcloud_scraper.main()
     return {"status": "ok"}
